66ec7ec0c2700ca5d7d5c75ebbd8b48c6e1cd6c3
f-regal <faysalregal@gmail.com>
2021-12-16 19:25:53 +0000

    Made Progress With Header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import classes from './Header.module.css'
+import burgers from '../UI/Images/burgersPic.jpg';
 
 const Header = () => {
 
@@ -11,6 +12,19 @@ const Header = () => {
                 <h1 className={classes.title}>ReactMeals</h1>
                 <button className={classes.cartBtn}>Cart</button>
             </header>
+
+            <div className={classes.container}>
+                <img src={burgers} alt="Burger" />
+                <img src={burgers} alt="Burger" />
+                <img src={burgers} alt="Burger" />
+                <img src={burgers} alt="Burger" />
+            </div>
+
+            <div className={classes.mealintro}>
+                <h1 className={classes.mealintrotitle}>Delicious Food Deliverd, To You</h1>
+                <p className={classes.mealintroinfo}>It’s been 10 years now and we like to think we’ve gotten pretty good at burgers and chips. We’re all about top quality beef, the best potatoes and good times. Don’t just take our word for it though, we’ll let our food and people do the talking. After all, we’re Just Being Honest…</p>
+            </div>
+
         </React.Fragment>
     )
 }
