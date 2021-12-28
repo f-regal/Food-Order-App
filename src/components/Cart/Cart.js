94cc711b4b826785525ebe98127421a390f11661
f-regal <faysalregal@gmail.com>
2021-12-28 17:53:06 +0000

    updated cart modal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,8 +1,29 @@
-import classes from './Cart.module.css'
+import classes from './Cart.module.css';
+import CartItem from './CartItem';
 
 const Cart = (props) => {
+
+    const NEW_MEALS = [
+      {id: 1,
+      name: "Burger1",
+      price: 21.99
+      },
+      {id: 2,
+      name: "Burger2",
+      price: 22.99
+      }
+  ];
+
+  
     return (
      <div className={classes.cart}>
+       <ul>
+          {NEW_MEALS.map(newmeal => <CartItem name={newmeal.name} id={newmeal.id} price={newmeal.price}></CartItem>)}
+       </ul>
+       <div className={classes.total}>
+        <h3 className={classes.totalTitle}>Total</h3>
+        <div className={classes.totalPrice}>$25</div>
+       </div>
      </div>
     );
   };
---
+++ src/components/Cart/CartItem.js
@@ -0,0 +1,29 @@
+import { Fragment } from 'react';
+import classes from './CartItem.module.css';
+const CartItem = (props) => {
+  
+    const price = `$${props.price}`;
+
+    return (
+        <Fragment>
+          <li className={classes.cartitem}>
+            <div>
+              <h3>{props.name}</h3>
+              <div className={classes.price}>{price}</div>
+            </div>
+            <div>
+              <input className={classes.amount} type="text" id="amount" name="amount" value={"x 2"} readonly></input>
+            </div>
+            <div className={classes.plusminus}>
+                <button className={classes.plus}>+</button>
+                <button className={classes.minus}>-</button>
+            </div>
+          </li>
+          <div>
+
+          </div>
+        </Fragment>
+    );
+  };
+  
+export default CartItem;
